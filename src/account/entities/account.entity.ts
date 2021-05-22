5abf4df25b46fb229d7ee46216d4d31a6947d803
Carlos <carlos.ribeiro.brito@hotmail.com>
2021-05-22 03:16:38 -0300

    add account entity

---
+++ src/account/entities/account.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { TransactionEntity as Transaction } from '../../transactions/entities';
+
+@Entity()
+export class AccountEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userEmail: string;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  accountNumber: string;
+
+  @Column('date')
+  operationDate!: string;
+
+  @Column('float')
+  firstDeposit: number;
+
+  @Column('float')
+  firstTax: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => Transaction, (trans) => trans.account)
+  transactions: Transaction[];
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
