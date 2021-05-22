f9378def52d87cc8aff8fc46f58e47b361d609b2
Carlos <carlos.ribeiro.brito@hotmail.com>
2021-05-22 02:56:35 -0300

    configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './config';
 
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [
+    GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: config.PG_URI,
+      port: config.PG_PORT,
+      username: config.PG_USERNAME,
+      password: config.PG_PASSWORD,
+      database: config.PG_DATABASE,
+      entities: ['dist/**/*.entity.ts'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
