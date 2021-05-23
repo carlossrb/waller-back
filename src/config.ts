e0c174a043ff9d63d33b51be8358b93f8e656055
Carlos <carlos.ribeiro.brito@hotmail.com>
2021-05-23 17:48:33 -0300

    change pg_uri

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  PG_URI: 'projetos.cmzjkhbg8uyr.us-east-1.rds.amazonaws.com',
+  PG_URI: 'postgres://carlosmagno:Stratovarius@projetos.cmzjkhbg8uyr.us-east-1.rds.amazonaws.com:5432',
   PG_PORT: 5432,
   PG_USERNAME: 'carlosmagno',
   PG_PASSWORD: 'Stratovarius',
