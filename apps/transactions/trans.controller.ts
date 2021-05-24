84ff13ecd5927f0d20375d848bce24f862371cd4
Carlos <carlos.ribeiro.brito@hotmail.com>
2021-05-24 01:36:11 -0300

    new trans controller methods

--- apps/transactions/trans.controller.ts
+++ apps/transactions/trans.controller.ts
@@ -1,29 +1,49 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { TransactionEntity, TransactionLogEntity, AccountEntity } from './entities';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { AccountEntity } from './entities';
 import { TransService } from './trans.service';
 
 @Controller()
 export class TransController {
-  constructor(
-    @InjectRepository(TransactionEntity)
-    private readonly transRepo: Repository<TransactionEntity>,
-
-    @InjectRepository(AccountEntity)
-    private readonly accountRepo: Repository<AccountEntity>,
-
-    @InjectRepository(TransactionLogEntity)
-    private readonly logRepo: Repository<TransactionLogEntity>,
-
-    private readonly transService: TransService
-  ) {}
+  constructor(private readonly transService: TransService) {}
 
   @Get(':id')
   async getAccountBalance(@Param('id') id: string): Promise<AccountEntity> {
     const acc = await this.transService.getBalance(id);
     if (!acc) {
       throw new NotFoundException('Transaction not found');
+    }
+    return acc;
+  }
+
+  @Post(':id/payment')
+  async payment(
+    @Param('id') id: string,
+    @Body() paymentData: { target: string; amount: string }
+  ): Promise<AccountEntity> {
+    const { target, amount } = paymentData;
+    const acc = await this.transService.makePayment(id, target, amount);
+    if (!acc) {
+      throw new NotFoundException('Payment not completed');
+    }
+    return acc;
+  }
+
+  @Post(':id/deposit')
+  async deposit(@Param('id') id: string, @Body() depositData: { amount: string }): Promise<AccountEntity> {
+    const { amount } = depositData;
+    const acc = await this.transService.makeDeposit(id, amount);
+    if (!acc) {
+      throw new NotFoundException('Deposit not completed');
+    }
+    return acc;
+  }
+
+  @Post(':id/withdrawal')
+  async withdrawal(@Param('id') id: string, @Body() withdrawalData: { amount: string }): Promise<AccountEntity> {
+    const { amount } = withdrawalData;
+    const acc = await this.transService.makeWithdrawal(id, amount);
+    if (!acc) {
+      throw new NotFoundException('Withdrawal not completed');
     }
     return acc;
   }
