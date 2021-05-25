e555067c872360a461c297bc0045a3fd07c776f6
Carlos <carlos.ribeiro.brito@hotmail.com>
2021-05-25 12:44:27 -0300

    fix configs

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const config = {
   PG_URI: 'postgres://carlosmagno:Stratovarius@projetos.cmzjkhbg8uyr.us-east-1.rds.amazonaws.com:5432',
- };
+  TRANSACTIONS_SERVICE_URL: 'http://localhost:4000/transactions',
+};
