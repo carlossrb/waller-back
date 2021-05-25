ddb75ec4a0a4ae094191e7ccb6cf4b4b2719c9e7
carlossrb <43443056+carlossrb@users.noreply.github.com>
2021-05-24 22:15:52 -0300

    Remove unnecessary configs

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,3 @@
 export const config = {
   PG_URI: 'postgres://carlosmagno:Stratovarius@projetos.cmzjkhbg8uyr.us-east-1.rds.amazonaws.com:5432',
-  PG_PORT: 5432,
-  PG_USERNAME: 'carlosmagno',
-  PG_PASSWORD: 'Stratovarius',
-  PG_DATABASE: 'waller',
-  TRANSACTIONS_SERVICE_URL: 'http://localhost:4000/transactions',
-};
+ };
